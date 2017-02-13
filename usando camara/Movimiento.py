25bf8540d1f49feab28e81fcb754fbae1e67134d
fkrazy <f2kloa@gmail.com>
2017-02-12 19:09:09 -0600

    punto trabado
    error en imagen de tamaño incorrecto,

--- usando camara/Movimiento.py
+++ usando camara/Movimiento.py
@@ -1,27 +1,29 @@
 def corte(linea):
     EsImagen=False
-    j=0
     punto=[]
-    for i in linea:
-        if linea[j]!= 0 and EsImagen==False:
-            punto.append(j)
+    for i in range(0,len(linea)):
+        if linea[i]!= 0 and EsImagen==False:
+            punto.append(i)
             EsImagen=True
-        elif linea[j]==0 and EsImagen==True:
-            punto.append(j)
+        elif linea[i]==0 and EsImagen==True:
+            punto.append(i)
             EsImagen=False
-        j+=1
     return punto
 
 def EncontrarEsquinas(imagen, esquinas=[]):
     linea=imagen.sum(axis=(1,2))
-    tam= esquinas.count
     esquinas.append(corte(linea))
-    esquinas.append(-1)
-    imagen=imagen.T
-    if esquinas.count <= tam+2:
-        esquinas.append(corte(imagen))
-    else:
-        esquinas.append(EncontrarEsquinas(imagen,esquinas))
-    
+    for i in range(0,len(esquinas[-1]),2):
+        if esquinas[-1].count != 1:
+            subimagen= imagen[esquinas[-1][i]:esquinas[-1][i+1],:,:]                
+        else:
+            subimagen=imagen[esquinas[-1][i]:,:,:]
+        linea=subimagen.sum(axis=(0,2))
+        if esquinas[-1] == 1:            
+            esquinas.append(corte(linea))
+            if esquinas[-1] != 1:
+                esquinas.append(EncontrarEsquinas(subimagen,esquinas))
+        else:
+            esquinas.append(EncontrarEsquinas(subimagen,esquinas))
     return esquinas
     
