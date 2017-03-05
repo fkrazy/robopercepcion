11c847899302a9fbecdebcc91b41a2e0f8219bb0
fkrazy <f2kloa@gmail.com>
2017-03-05 13:42:29 -0600

    disminuir lineas cuadros
    disminuy toda la funcion encontrar cuadros a hacerlo directo desde
    encontrar esquinas, ahora solo queda de adorno

--- usando camara/MostrandoImagenPrueba.py
+++ usando camara/MostrandoImagenPrueba.py
@@ -1,15 +1,12 @@
 import cv2
 import Movimiento
 imagenes=[]
-im=cv2.imread("movimiento.png")
-imagenes.append(im)
-im=cv2.imread("negro.png")
-imagenes.append(im)
-im=cv2.imread("movimiento.png")
+im=cv2.imread("movimiento2.png")
 imagenes.append(im)
 for j in imagenes:
     cuadro=Movimiento.EncontrarCuadros(j,rango=0)
     im=j
+    k=255
     for i in cuadro:       
-        cv2.rectangle(im, (i[1],i[0]),  ( i[3],i[2]), (0, 255, 0), 2 )
+        cv2.rectangle(im, (i[1],i[0]),  ( i[3],i[2]), (0, k, 0), 2 )
     cv2.imshow("imagen",im)
--- usando camara/Movimiento.py
+++ usando camara/Movimiento.py
@@ -1,3 +1,6 @@
+import cv2
+anchoImagen=480
+altoImagen=640
 def corte(linea,rango=20):
     EsImagen=False
     punto=[]
@@ -13,11 +16,12 @@ def corte(linea,rango=20):
             conteo+=1
     return punto
      
-def EncontrarEsquinas(imagen,esquinas=[],rango=20):
+def EncontrarEsquinas(imagen,esquinas=[],rango=20,cuadros=[[0,0,anchoImagen,altoImagen]],columna=0):
     linea=imagen.sum(axis=(1))
     temesquinas=[]
     temesquinas=corte(linea,rango)
     for i in range(0,len(temesquinas),2):
+        x_y=columna%2
         if len(temesquinas)!=1:
             if len(temesquinas) != i+1:
                 lado=temesquinas[i]
@@ -28,36 +32,29 @@ def EncontrarEsquinas(imagen,esquinas=[],rango=20):
             subimagen = imagen[temesquinas[i]:, :]
             tem=corte(linea,rango)
             if len(tem)==1:
-                temesquinas[0]=-1
-                esquinas.append(temesquinas)
-                break
-        esquinas= EncontrarEsquinas(subimagen.T,esquinas,rango)   
+                if tem[0]==0:
+                    temesquinas[0]=-1
+                    esquinas.append(temesquinas)  
+                    
+                else:
+                    temesquinas[0]=tem[0]
+                    temesquinas.append(-1)
+                    esquinas.append([temesquinas[1]])                    
+                    esquinas.append([temesquinas[0]])                    
+                    cuadros[-1][x_y]+=tem[0]
+                cuadros.append([0,0,anchoImagen,altoImagen])                
+                break       
+        cuadros[-1][x_y]+=temesquinas[i]
+        if i+1!= len(temesquinas):
+            cuadros[-1][x_y+2]=temesquinas[i+1]-temesquinas[i]+cuadros[-1][x_y]
+        columna+=1
+        esquinas,cuadros= EncontrarEsquinas(subimagen.T,esquinas,rango,cuadros,columna)   
         esquinas.append(temesquinas[i:i+2])
-    return esquinas
+    return esquinas,cuadros
 
 def EncontrarCuadros(imagen,rango=20):
     movBidi=imagen.sum(axis=2)
-    esquinas=EncontrarEsquinas(movBidi,esquinas=[],rango=rango)
-    cuadros=[]    
-    if esquinas!= []:
-        cuadros.append([0,0,len(movBidi),len(movBidi.T)])
-    esX=True
-    for i in range(len(esquinas)-1,0,-1):
-        #el carro corre hasta el final del vector
-        if esquinas[i][0]==-1:
-            cuadros.append([0,0,len(movBidi),len(movBidi.T)])
-            #x,y,ancho,alto
-            esX=True 
-        elif esX:
-            if len(esquinas[i])%2==0:
-                cuadros[-1][2]=esquinas[i][1]
-            cuadros[-1][0]+=esquinas[i][0]
-            esX=False
-        else:
-            if len(esquinas[i])%2==0:
-                cuadros[-1][3]=esquinas[i][1]
-            cuadros[-1][1]+=esquinas[i][0]
-            esX=True
-    return cuadros     
+    esquinas,cuadros=EncontrarEsquinas(movBidi,esquinas=[],rango=rango)
+    return cuadros
             
 
